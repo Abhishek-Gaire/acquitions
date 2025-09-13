bd93350c57245a6a5c3fa50b2523934fb72dbc9c
Abhishek Gaire <abhisekgaire7@gmail.com>
2025-09-13 16:42:15 +0545

    implement auth

--- src/app.js
+++ src/app.js
@@ -1,10 +1,38 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import authRoutes from "#routes/auth.routes.js";
+import logger from "#config/logger.js";
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
+
+app.use(morgan('combined', {
+    stream: {
+        write:(message) => logger.info(message.trim())
+    }
+}));
 
 app.get('/', (req, res) => {
+    logger.info('Hello from acquisition')
   res.status(200).send('Hello from Acquisitions API');
 });
+app.get('/health', (req, res) => {
+    res.status(200).json({
+    status: 'OK',
+        timestamp:new Date().toISOString(),
+        uptime:process.uptime()
+    })
+})
+app.use('/api/v1/auth', authRoutes)
 
 export default app;
